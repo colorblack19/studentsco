cf07541d4217371474959f713045d0710875c97e
system@school <audibertaudibert5@gmail.com>
2025-12-25 17:07:40 +0300

    add and change appy.py

--- students/apps.py
+++ students/apps.py
@@ -1,8 +1,8 @@
 from django.apps import AppConfig
-from pathlib import Path
-
 
 class StudentsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'students'
-    path = Path(__file__).resolve().parent
+
+    def ready(self):
+        import students.signals
