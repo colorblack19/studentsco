fea210825a072c1f6c664d5b643c0c249e79ba06
system@school <audibertaudibert5@gmail.com>
2025-12-26 07:06:12 +0300

    test deploy change

--- accounts/apps.py
+++ accounts/apps.py
@@ -1,8 +1,2 @@
-from django.apps import AppConfig
-from pathlib import Path
 
 
-class AccountsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'accounts'
-    path = Path(__file__).resolve().parent
